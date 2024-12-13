4122719f2091ae72114d018db9ab42398c8babba
andrawicaksono <andrawicaksono14@gmail.com>
2024-12-14 05:58:14 +0700

    fix: hide header

--- App.js
+++ App.js
@@ -2,51 +2,25 @@ import "react-native-gesture-handler";
 import { enableScreens } from "react-native-screens";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { SafeAreaView, StyleSheet, useColorScheme } from "react-native";
-import {
-  NavigationContainer,
-  DefaultTheme,
-  DarkTheme,
-} from "@react-navigation/native";
+import { SafeAreaView, StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
-
 enableScreens();
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const colorScheme = useColorScheme(); // Detect system theme
-
   return (
-    <SafeAreaProvider>
-      <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
-      <NavigationContainer
-        theme={colorScheme === "dark" ? DarkTheme : DefaultTheme}
-      >
-        <SafeAreaView
-          style={[
-            styles.container,
-            colorScheme === "dark" && styles.darkBackground,
-          ]}
-        >
-          <Stack.Navigator initialRouteName="Home">
-            <Stack.Screen
-              name="Home"
-              component={HomeScreen}
-              options={{
-                title: "Home",
-                headerStyle: {
-                  backgroundColor:
-                    colorScheme === "dark" ? "#1f1f1f" : "#fafbfd",
-                },
-                headerTintColor: colorScheme === "dark" ? "#fff" : "#000",
-              }}
-            />
-          </Stack.Navigator>
-        </SafeAreaView>
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Home", headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -54,8 +28,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fafbfd",
-  },
-  darkBackground: {
-    backgroundColor: "#121212",
   },
 });
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -7,23 +7,28 @@ import TransactionHistory from "../components/TransactionHistory";
 
 import photo from "../assets/photo.jpeg";
 import BottomAppBar from "../components/BottomAppBar";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
 
 const HomeScreen = () => {
   return (
-    <>
-      <ScrollView>
-        <AppBar
-          fullName="Chelsea Immanuela"
-          accountType="Personal Account"
-          photo={photo}
-        />
-        <Greeting firstName="Chelsea" />
-        <DetailRow label="Account No." value="100899" />
-        <BalanceDetail balance="Rp10.000.000" />
-        <TransactionHistory />
-      </ScrollView>
-      <BottomAppBar />
-    </>
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <StatusBar style="auto" />
+        <ScrollView>
+          <AppBar
+            fullName="Chelsea Immanuela"
+            accountType="Personal Account"
+            photo={photo}
+          />
+          <Greeting firstName="Chelsea" />
+          <DetailRow label="Account No." value="100899" />
+          <BalanceDetail balance="Rp10.000.000" />
+          <TransactionHistory />
+        </ScrollView>
+        <BottomAppBar />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 };
 
